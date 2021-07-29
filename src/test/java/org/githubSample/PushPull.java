adcf7034049e1336951dd39e7871b33b82dd97dd
kalpanateddy <jeenukalpana@gmail.com>
2021-07-29 22:42:10 +0530

    git pgt

---
+++ src/test/java/org/githubSample/PushPull.java
@@ -0,0 +1,23 @@
+package org.githubSample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PushPull {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspace\\DataDriven1\\drive\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	WebElement txtUserName = driver.findElement(By.id("email"));
+	txtUserName.sendKeys("happy");
+	WebElement userName = driver.findElement(By.id("pass"));
+	userName.sendKeys("password");
+	WebElement login = driver.findElement(By.name("login"));
+	login.click();
+	
+
+}}
