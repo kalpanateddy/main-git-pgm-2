12376094c5a46d008fd2141e139955a1f3033cdd
kalpanateddy <jeenukalpana@gmail.com>
2021-07-31 08:50:07 +0530

    pull works

--- src/test/java/org/githubSample/PushPull.java
+++ src/test/java/org/githubSample/PushPull.java
@@ -19,5 +19,8 @@ public class PushPull {
 	WebElement login = driver.findElement(By.name("login"));
 	login.click();
 	
+	System.out.println("pull activities");
+	System.out.println("done");
+	
 
 }}
